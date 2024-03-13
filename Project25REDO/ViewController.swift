abad16cc06ee008ea0e62f899172dfe4c2f3196b
Andy Park <woong1227@gmail.com>
2024-03-12 20:55:34 -0700

    Initial Commit

---
+++ Project25REDO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project25REDO
+//
+//  Created by Andy Park on 3/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
